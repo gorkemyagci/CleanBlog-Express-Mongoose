bc1f7e49aee6dce3fb5af081dcbacc014edbfe81
gorkemyagci <gorkemyagci705@gmail.com>
2023-09-29 03:17:27 +0300

    mongoose-express

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
 const app = express();
 const Blog = require('./models/Blog');
+const fs = require('fs');
 
 const port = 3000;
 
@@ -13,17 +15,44 @@ mongoose.connect('mongodb://localhost/cleanblog-test-db', {
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
-app.get('/', (req, res) => {
-    res.status(200).type('text/plain').send('Hello World');
+app.get('/', async (req, res) => {
+    const blogs = await Blog.find({});
+    for (let i = 0; i < blogs.length; i++) {
+        res.write(
+            `
+        <div>
+        <h3>${blogs[i].title}</h3>
+        <p>${blogs[i].description}</p>
+        <img style="width:300px" src="${blogs[i].image}" alt="image" />
+        </div>
+        `
+        );
+    }
+    res.end();
 });
 
 app.post('/blog', async (req, res) => {
     try {
-        const newBlog = await Blog.create(req.body);
-        res.status(200).type('text/plain').send(newBlog);
+        const uploadDir = __dirname + '/public/uploads/';
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
+        }
+        let uplaodedImage = req.files.image;
+        let uploadPath = __dirname + '/public/uploads/' + uplaodedImage.name;
+        uplaodedImage.mv(uploadPath, async () => {
+            await Blog.create({
+                ...req.body,
+                image: '/uploads/' + uplaodedImage.name,
+            })
+        })
+        res.status(200).type('text/plain').send({
+            ...req.body, image: uplaodedImage.name
+        });
+        res.end();
     } catch (error) {
-        res.status(500).json({ error: 'An error occurred while creating the blog.' });
+        console.log(error);
     }
 });
 
--- models/Blog.js
+++ models/Blog.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const BlogSchema = new Schema({
     title: String,
     description: String,
+    image: String,
     createdDate: {
         type: Date,
         default: Date.now,
